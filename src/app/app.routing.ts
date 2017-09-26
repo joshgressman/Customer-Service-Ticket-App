e3bf6b2a4d62c46930f1471af5ce26de82d2ff6e
joshgressman <joshgressman@gmail.com>
2017-09-26 16:44:46 -0500

    added routing

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,8 +1,10 @@
 
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
 const APP_ROUTES: Routes = [
-
+ {path: '', redirectTo: '/home', pathMatch: 'full'},
+ {path: 'home', component: HomeComponent}
 ]
 
 export const routing = RouterModule.forRoot(APP_ROUTES);
